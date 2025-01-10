d94849d1fa18886a35100a2402ced23f7e41ec5b
Varatharaj K <varatharajk@Mac.lan>
2025-01-10 23:17:38 +0530

    Token Address

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 export const NEXT_PUBLIC_NETWORK = process.env.NEXT_PUBLIC_NETWORK || 'testnet';
 export const SIN_STAKING_CONTRACT = process.env.SIN_STAKING_CONTRACT || 'sinstaking.mintspace2.testnet';
-export const SIN_STAKING_CONTRACT_BALANCE = process.env.SIN_STAKING_CONTRACT_BALANCE || 'sin-test-tkn.testnet';
+export const SIN_STAKING_CONTRACT_BALANCE = process.env.SIN_STAKING_CONTRACT_BALANCE || 'sin-test-tkn1.testnet';
 export const SIN_STAKING_CONTRACT_TOKEN_STAKE = process.env.SIN_STAKING_CONTRACT_TOKEN_STAKE || 'sin-test-tkn1.testnet';
 export const SIN_STAKING_CONTRACT_NFT_STAKE = process.env.SIN_STAKING_CONTRACT_NFT_STAKE || 'sin-nft-contract-account23.testnet';
 export const SIN_STAKING_CONTRACT_NFT_STAKE_INFO = process.env.SIN_STAKING_CONTRACT_NFT_STAKE_INFO || 'sin-nft-contract-account23.testnet';
