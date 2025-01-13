25158ef46f4b61c3ea08efcd1c54b632888e77b8
SharmilaBlessyS <sharmila@kalakendradao.org>
2025-01-13 20:57:59 +0530

    new configuration changes added

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
-export const NEXT_PUBLIC_NETWORK = process.env.NEXT_PUBLIC_NETWORK || 'mainnet';
-export const SIN_STAKING_CONTRACT = process.env.SIN_STAKING_CONTRACT || 'sin-ft-staking.near';
-export const SIN_STAKING_CONTRACT_BALANCE = process.env.SIN_STAKING_CONTRACT_BALANCE || 'sin-339.meme-cooking.near';
-export const SIN_STAKING_CONTRACT_TOKEN_STAKE = process.env.SIN_STAKING_CONTRACT_TOKEN_STAKE || 'sin-ft-staking.near';
-export const SIN_STAKING_CONTRACT_NFT_STAKE = process.env.SIN_STAKING_CONTRACT_NFT_STAKE || 'sin-nft-staking.near';
-export const SIN_STAKING_CONTRACT_SHOW_NFTS = process.env.SIN_STAKING_CONTRACT || 'sin-nft.bodega-lab.near';
+export const NEXT_PUBLIC_NETWORK = "mainnet";
+export const SIN_STAKING_CONTRACT = "sin-ft-staking.near";
+export const SIN_STAKING_CONTRACT_BALANCE = "sin-339.meme-cooking.near";
+export const SIN_STAKING_CONTRACT_TOKEN_STAKE = "sin-ft-staking.near";
+export const SIN_STAKING_CONTRACT_NFT_STAKE = "sin-nft-staking.near";
+export const SIN_STAKING_CONTRACT_SHOW_NFTS = "sin-nft.bodega-lab.near";
